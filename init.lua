d04ef5b71d58b2321bf5945581da92e3e8e670dd
Gabriel Marko <marko.gabriel.cz@gmail.com>
2025-10-16 17:48:59 +0200

    Allow bashls.

--- init.lua
+++ init.lua
@@ -675,6 +675,7 @@ require('lazy').setup({
         gopls = {},
         pyright = {},
         perlnavigator = {},
+        bashls = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
