d645e9ffdcbeda2874bfb98274b9f99b9e9198bb
suic86 <marko.gabriel.cz@gmail.com>
2025-10-16 14:26:40 +0200

    Activate autoformatting for Python and fix timout issues.

--- init.lua
+++ init.lua
@@ -761,7 +761,7 @@ require('lazy').setup({
           return nil
         else
           return {
-            timeout_ms = 500,
+            timeout_ms = 5000,
             lsp_format = 'fallback',
           }
         end
@@ -769,8 +769,7 @@ require('lazy').setup({
       formatters_by_ft = {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
-        --
+        python = { 'isort', 'black' },
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
       },
