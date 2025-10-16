b97038be86b75f346e26319fa6aca99caeb104a3
suic86 <marko.gabriel.cz@gmail.com>
2025-10-16 16:11:55 +0200

    Set tabstops and expandtab.

--- init.lua
+++ init.lua
@@ -91,7 +91,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.o`
@@ -1010,6 +1010,11 @@ require('lazy').setup({
     },
   },
 })
+
+-- vim.o.tabstop = 4
+-- vim.o.softtabstop = 4
+-- vim.o.expandtab = true
+-- vim.o.shiftwidth = 4
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,10 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.expandtab = true
+vim.o.shiftwidth = 4
+
 return {}
